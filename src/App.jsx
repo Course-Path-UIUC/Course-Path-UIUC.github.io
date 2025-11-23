ecea482e3d62ff08720c92a8e2fcb4940ebd7601
Kristin Wiseman <88798340+kristin-wiseman@users.noreply.github.com>
2025-11-23 12:23:26 -0600

    Add React to project, remove boilerplate counter component.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './style.css'
+
+const App = () => {
+  return (
+    <>
+      <main className='p-8'>
+        <h1>Course Path</h1>
+        <p>Course path applies topological graph sorting to a list of classes to display them in an order that satisfies prerequisites.</p>
+      </main>
+      <footer className='p-8 bg-slate-200'>
+        Made with 💙 by: Aastha - Daniel - Kristin
+      </footer>
+    </>
+  );
+};
+
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 /** @type {import('vite').UserConfig} */
+import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite';
 
 export default {
   plugins: [
+    react(),
     tailwindcss(),
   ],
 }
