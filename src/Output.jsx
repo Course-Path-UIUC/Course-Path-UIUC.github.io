53122fc74bdbea4e2f5a583e7362637dd1708708
Kristin Wiseman <88798340+kristin-wiseman@users.noreply.github.com>
2025-11-27 16:14:39 -0600

    Feat: style output courses as chips, limit main elem width.

--- src/Output.jsx
+++ src/Output.jsx
@@ -1,13 +1,29 @@
 
 const Output = ({ selectedCourses }) => {
+  const CHIP_BASE_CLASSES = [
+    'px-3.5',
+    'py-2',
+    'border',
+    'border-blue-600',
+    'rounded-full',
+    'bg-slate-100',
+    'font-medium',
+    'text-blue-800',
+    'dark:text-white',
+    'dark:bg-blue-900',
+  ]
+
   const listItems = selectedCourses.map(course =>
-    <li key={course}>{course}</li>
+    // course is a string with the department prefix and course number.
+    <li key={course} className={CHIP_BASE_CLASSES.join(' ')}>{course}</li>
   );
 
   return (
     <output>
       {selectedCourses.length > 0 ? (
-        <ol>{listItems}</ol>
+        <ol className='flex flex-col flex-wrap gap-4 content-center sm:flex-row'>
+          {listItems}
+        </ol>
       ) : ''}
     </output>
   );
